b05341d279b592e464592afe86605f7c83f186b3
suifengLuo <772396555@qq.com>
2015-07-14 18:26:41 +0800

    begin

---
+++ hutu.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"text/template"
+)
+
+type Server struct {
+	ServerName string
+	ServerIP   string
+}
+
+func SayHello(rsp http.ResponseWriter, req *http.Request) {
+	s := Server{ServerName: "localhost", ServerIP: "127.0.0.1"}
+	str, _ := json.Marshal(s)
+	rsp.Write(str)
+}
+
+func ApiListView(rsp http.ResponseWriter, req *http.Request) {
+	t, _ := template.ParseFiles("src/hutu/views/api_list.html")
+	t.Execute(rsp, nil)
+}
+
+func main() {
+	http.HandleFunc("/apilist/", ApiListView)
+	http.HandleFunc("/say", SayHello)
+	http.ListenAndServe(":8001", nil)
+}
